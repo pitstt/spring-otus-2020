4e30f3b267f5e537653f7916b8c9131d844898c5
Подставкин Артем Алексеевич <aapodsta@mts.ru>
2020-09-28 15:18:48 +0300

    исправление замечаний

--- hw03-spring-boot/src/test/java/ru/otus/hw03springboot/dao/QuestionDaoSimpleTest.java
+++ hw03-spring-boot/src/test/java/ru/otus/hw03springboot/dao/QuestionDaoSimpleTest.java
@@ -1,45 +1,21 @@
 package ru.otus.hw03springboot.dao;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
+@SpringBootTest
 class QuestionDaoSimpleTest {
 
-    private QuestionDaoSimple questionDao;
-
-    private Reader reader;
-
-    private CSVFormat csvFormat;
-
-    @BeforeEach
-    void setUp() {
-        csvFormat = mock(CSVFormat.class);
-        reader = mock(InputStreamReader.class);
-        questionDao = new QuestionDaoSimple(reader, csvFormat);
-    }
+    @Autowired
+    private QuestionDao questionDao;
 
     @Test
     void getAllQuestion() throws IOException {
-        CSVParser parser = mock(CSVParser.class);
-        CSVRecord record = mock(CSVRecord.class);
-
-        given(csvFormat.parse(reader)).willReturn(parser);
-        given(parser.getRecords()).willReturn(Collections.singletonList(record));
-        given(record.get("text")).willReturn("2+2=?");
-        given(record.get("answer")).willReturn("4");
-
         assertEquals(1, questionDao.getAllQuestion().size());
     }
 }
